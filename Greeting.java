47c180eb3af861a20c7292bf47648d0b4ba80e15
Argha Digar <129493815+IT-Argha@users.noreply.github.com>
2023-09-02 13:17:04 +0530

    Update Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -1,3 +1,7 @@
+        // Q: 1
+        // Author: Argha Digar
+        // Title: Greeting
+        // Description: This program prints a greeting message.
 public class Greeting {
     public static void main(String[] args) {
         System.out.println("Hello Everyone!! My name is Argha Digar.");
