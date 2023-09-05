cd8ba37583768402fc999c6d9add74f06e5e173b
Argha Digar <129493815+IT-Argha@users.noreply.github.com>
2023-09-05 18:57:23 +0530

    Create VariablesExample.java

---
+++ VariablesExample.java
@@ -0,0 +1,12 @@
+// Q: 2
+// AUTHOR: Argha Digar
+// TITLE: VariablesExample
+// DESCRIPTION: This Java program demonstrates the use of variables to store and display information such as name, age, and salary.
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 23;
+        double salary = 50000.50;
+        String name = "Argha";
+        System.out.println("Name: " + name + ", Age: " + age + ", Salary: " + salary);
+    }
+}
